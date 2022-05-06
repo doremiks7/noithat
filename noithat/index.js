f76659ae4bb6d45cb4450f8ed492ccbb612d4295
nvantruong <nvantruong@vnpt.vn>
2022-05-06 13:22:51 +0700

    fsdf

---
+++ noithat/index.js
@@ -0,0 +1,62 @@
+let genHtml = "<div class=\"col-md-4 col-sm-6 col-xs-6 pro-loop \">\n" +
+    "    <div class=\"product-block product-resize \">\n" +
+    "        <div class=\"product-img \">\n" +
+    "            <a href=\"/products/giay-vai-dan-tuong-ma-0101\" title=\"Giấy vải dán tường ( Mã 0101 )\"\n" +
+    "                class=\"image-resize ratiobox\">\n" +
+    "                <picture>\n" +
+    "                    <source media=\"(max-width: 480px)\"\n" +
+    "                        data-srcset=\"image/product/0101_850c912f03f94834a0c322ede5d34e7a_medium.jpg\"\n" +
+    "                        srcset=\"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=\" />\n" +
+    "                    <source media=\"(min-width: 481px) and (max-width: 767px)\"\n" +
+    "                        data-srcset=\"image/product/0101_850c912f03f94834a0c322ede5d34e7a_large.jpg\"\n" +
+    "                        srcset=\"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=\" />\n" +
+    "                    <source media=\"(min-width: 768px)\"\n" +
+    "                        data-srcset=\"image/product/0101_850c912f03f94834a0c322ede5d34e7a_grande.jpg\"\n" +
+    "                        srcset=\"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=\" />\n" +
+    "                    <img class=\"lazyload img-loop\" data-sizes=\"auto\"\n" +
+    "                        data-src=\"image/product/0101_850c912f03f94834a0c322ede5d34e7a_grande.jpg\"\n" +
+    "                        data-lowsrc=\"image/product/0101_850c912f03f94834a0c322ede5d34e7a_grande.jpg\"\n" +
+    "                        src=\"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=\"\n" +
+    "                        alt=\" Giấy vải dán tường ( Mã 0101 ) \" />\n" +
+    "                </picture>\n" +
+    "                <picture>\n" +
+    "                    <source media=\"(max-width: 480px)\"\n" +
+    "                        data-srcset=\"image/product/0101_f3315994ced144b49eaa35c53162ac5e_medium.jpg\"\n" +
+    "                        srcset=\"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=\">\n" +
+    "                    <source media=\"(min-width: 481px) and (max-width: 767px)\"\n" +
+    "                        data-srcset=\"image/product/0101_f3315994ced144b49eaa35c53162ac5e_large.jpg\"\n" +
+    "                        srcset=\"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=\">\n" +
+    "                    <source media=\"(min-width: 768px)\"\n" +
+    "                        data-srcset=\"image/product/0101_f3315994ced144b49eaa35c53162ac5e_grande.jpg\"\n" +
+    "                        srcset=\"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=\">\n" +
+    "                    <img class=\"img-loop img-hover lazyload\"\n" +
+    "                        data-src=\"image/product/0101_f3315994ced144b49eaa35c53162ac5e_grande.jpg\"\n" +
+    "                        src=\"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=\"\n" +
+    "                        alt=\" Giấy vải dán tường ( Mã 0101 ) \" />\n" +
+    "                </picture>\n" +
+    "            </a>\n" +
+    "            <div class=\"button-add hidden\">\n" +
+    "                <button type=\"submit\" title=\"Buy now\" class=\"action\" onclick=\"buy_now('1073726547')\">Mua ngay<i\n" +
+    "                        class=\"fa fa-long-arrow-right\"></i></button>\n" +
+    "            </div>\n" +
+    "            <div class=\"pro-price-mb\">\n" +
+    "\n" +
+    "                <span class=\"pro-price\">1,500,000₫</span>\n" +
+    "            </div>\n" +
+    "        </div>\n" +
+    "        <div class=\"product-detail clearfix\">\n" +
+    "            <div class=\"box-pro-detail\">\n" +
+    "                <h3 class=\"pro-name\">\n" +
+    "                    <a href=\"/products/giay-vai-dan-tuong-ma-0101\" title=\"Giấy vải dán tường ( Mã 0101 )\">\n" +
+    "                        Giấy vải dán tường ( Mã 0101 )\n" +
+    "                    </a>\n" +
+    "                </h3>\n" +
+    "                <div class=\"box-pro-prices\">\n" +
+    "                    <p class=\"pro-price \">\n" +
+    "                        <span>1,500,000₫</span>\n" +
+    "                    </p>\n" +
+    "                </div>\n" +
+    "            </div>\n" +
+    "        </div>\n" +
+    "    </div>\n" +
+    "</div>";
---
+++ noithat/load.js
@@ -0,0 +1,3 @@
+function getHeader(){
+    html = "";
+}
